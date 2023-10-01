e27a084a98a2b21e6bb319c4c35065989a5f786d
evan <evandromariano49@gmail.com>
2023-10-01 13:19:32 -0300

    astronvim configs

---
+++ lua/user/community.lua
@@ -0,0 +1,22 @@
+return{
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.angular" },
+  { -- further customize the options set by the community
+  "catppuccin",
+  opts = {
+    integrations = {
+      sandwich = false,
+      noice = true,
+      mini = true,
+      leap = true,
+      markdown = true,
+      neotest = true,
+      cmp = true,
+      overseer = true,
+      lsp_trouble = true,
+      rainbow_delimiters = true,
+    },
+  },
+},
+}
